d3c726a4dfe78ce1ca280ba9cd6aca2c0dd16a1b
nanto-work <nanto@ya.ru>
2020-05-24 20:20:58 +0300

    gwent upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
 
-	$("#main-carousel1").slick({
-		infinite: false,
-		appendArrows: "#main-carousel1__nav"
-	});
-	
-	$("#main-carousel2").slick({
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		infinite: false,
-		appendArrows: "#main-carousel2__nav"
-	});
-	
-	$("#main-carousel3").slick({
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		infinite: false,
-		appendArrows: "#main-carousel3__nav"
-	});
 
 });
 
